41eaa388aa61758040f92c9e7e5f18000cd8bc7b
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-11-10 14:56:12 +0200

    setup deploy

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.get("/", (req, res) => res.status(200).send("Hello World!"));
+
+const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+server.keepAliveTimeout = 120 * 1000;
+server.headersTimeout = 120 * 1000;
+
--- utils/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    port: 3001,
-    mongoURI: process.env.MONGO_URI,
-    secret: process.env.SECRET_KEY,
-  };
