3c35a71ecaf619e7472a0270c1226900bea3c5c8
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-11-10 15:12:53 +0200

    connected to mongodb

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const connection = require("./utils/db");
+
 const app = express();
 const port = process.env.PORT || 3001;
+const uri = process.env.DATABASE_URL;
+connection(uri);
 
 app.get("/", (req, res) => res.status(200).send("Hello World!"));
 
